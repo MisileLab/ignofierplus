8dee81a0351161518e8dba2d3696a88da67895f9
MisileLaboratory <misileminecord@gmail.com>
2023-02-14 17:54:53 +0000

    new .gitignore and BSD License and other thing

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Base-repository
+# Ignofier+
 ![Rust](https://img.shields.io/badge/language-rust-1976d2?style=for-the-badge&logo=rust)
-![License](https://img.shields.io/badge/license-misilelab-green?style=for-the-badge)
+![License](https://img.shields.io/badge/license-bsd-green?style=for-the-badge)
+
+Rust port of [ignofier](https://github.com/sujang958/ignofier) and some improvements
+
+## Improvements
+
+1. Rust = Compiled Language so performance improvement
+2. Append gitignore file if .gitignore exist
+
+## Requirements
+
+- Git
+
+## Installation
+
+Just get binary in [github actions](https://github.com/MisileLab/ignofierplus/actions/workflows/build.yml)
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,91 @@
+use std::{
+  path::Path,
+  process::Command,
+  fs::{File, read_to_string, OpenOptions},
+  io::Write
+};
+use dirs::home_dir;
+use walkdir::WalkDir;
+use requestty::{question::{completions, Completions}, Question};
+
+fn auto_complete(p: String, p2: &Vec<String>) -> Completions<String> {
+  let mut p3 = Vec::<String>::new();
+  p3.push(p.clone());
+  for i in p2 {
+    if i.starts_with(&p) {
+      p3.push(i.to_string());
+    }
+  };
+  let mut _completions = completions!();
+  _completions.extend(p3);
+  return _completions;
+}
+
 fn main() {
-  println!("hello, miku!")
+  let mut selections = Vec::<String>::new();
+  let _binding = Path::new(&home_dir().unwrap()).join(".ignofierplus");
+  let pathforgit = _binding.to_str().unwrap();
+
+  if Path::new(&home_dir().unwrap()).join(".ignofierplus").is_dir() {
+    Command::new("git").args(["pull", pathforgit]);
+  } else {
+    Command::new("git").args(["clone", "https://github.com/github/gitignore", pathforgit]);
+  }
+
+  for entry in WalkDir::new(Path::new(&home_dir().unwrap()).join(".ignofierplus")) {
+    let entry = entry.unwrap();
+    if entry.path().to_str().unwrap().to_string().ends_with(".gitignore") {
+      selections.push(entry.path().to_str().unwrap().to_string().trim_start_matches(pathforgit).trim_start_matches("/").trim_start_matches("\\").to_string());
+    }
+  }
+
+  let ques = requestty::Question::input("a")
+    .message("Choose .gitignore template")
+    .auto_complete(|p, _| auto_complete(p, &selections))
+    .validate(|p, _| {
+      if _binding.join(p).exists() {
+          Ok(())
+      } else {
+          Err(format!("file `{}` doesn't exist", p))
+      }
+  })
+  .build();
+
+  let selection = requestty::prompt_one(ques).unwrap().try_into_string().unwrap();
+
+  if !Path::new(".gitignore").is_file() {
+    let mut file = File::create(".gitignore").unwrap();
+    file.write_all(format!("# {} by ignofierplus\n{}", selection.to_string(), read_to_string(format!("{pathforgit}/{selection}")).unwrap()).as_bytes()).unwrap();
+  } else {
+    let input = Question::select("theme")
+    .message(".gitignore exists, append or overwrite?")
+    .choices(vec!["append", "overwrite"])
+    .build();
+    let _selection = requestty::prompt_one(input).unwrap().try_into_list_item().unwrap().text;
+    let _append: bool;
+    if _selection == "append" {
+      _append = true;
+    } else {
+      _append = false;
+    }
+    let mut file = OpenOptions::new()
+      .write(true)
+      .append(_append)
+      .open(".gitignore")
+      .unwrap();
+
+    if !_append {
+      file.set_len(0).unwrap();
+    }
+    
+    if let Err(e) = writeln!(file, "{}", format!(
+      "# {} by ignofierplus\n{}", 
+      selection.to_string(), 
+      read_to_string(format!("{pathforgit}/{selection}")).unwrap().trim_end_matches("\n")
+    )) {
+      eprintln!("Couldn't write to file: {}", e);
+    }
+  }
+
+  println!("Your .gitignore ready!")
 }
