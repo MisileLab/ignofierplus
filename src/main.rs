c44034e1e532dc0e39dbf47e2ee5d30c856d7cfc
MisileLab <misileminecord@gmail.com>
2023-12-20 16:29:17 +0900

    simple refactoring

--- src/main.rs
+++ src/main.rs
@@ -38,21 +38,21 @@ fn comment_delete(a: String, b: bool) -> String {
 }
 
 fn main() {
+  let homedir = Path::new(&home_dir().unwrap()).join(".ignofierplus");
   let mut selections = Vec::<String>::new();
-  let _binding = Path::new(&home_dir().unwrap()).join(".ignofierplus");
-  let pathforgit = _binding.to_str().unwrap();
+  let pathforgit = homedir.to_str().unwrap();
   println!("{}", pathforgit);
 
-  if Path::new(&home_dir().unwrap()).join(".ignofierplus").is_dir() {
+  if homedir.is_dir() {
     let _original = env::current_dir().unwrap();
-    env::set_current_dir(_binding.clone()).unwrap();
+    env::set_current_dir(homedir.clone()).unwrap();
     Command::new("git").args(["pull"]).spawn().unwrap().wait().unwrap();
     env::set_current_dir(_original).unwrap();
   } else {
     Command::new("git").args(["clone", "https://github.com/github/gitignore", pathforgit]).spawn().unwrap().wait().unwrap();
   }
 
-  for entry in WalkDir::new(Path::new(&home_dir().unwrap()).join(".ignofierplus")) {
+  for entry in WalkDir::new(&homedir) {
     let entry = entry.unwrap();
     if entry.path().to_str().unwrap().to_string().ends_with(".gitignore") {
       selections.push(entry.path().to_str().unwrap().to_string().trim_start_matches(pathforgit).trim_start_matches('/').trim_start_matches('\\').to_string());
@@ -63,7 +63,7 @@ fn main() {
     .message("Choose .gitignore template")
     .auto_complete(|p, _| auto_complete(p, &selections))
     .validate(|p, _| {
-      if _binding.join(p).exists() {
+      if homedir.join(p).exists() {
           Ok(())
       } else {
           Err(format!("file `{}` doesn't exist", p))
@@ -72,12 +72,9 @@ fn main() {
   .build();
 
   let selection = requestty::prompt_one(ques).expect("no selected string").try_into_string().expect("can't make select to string");
-
-  if !Path::new(".gitignore").is_file() {
-    let mut file = File::create(".gitignore").unwrap();
-    file.write_all(format!("\n# {} by ignofierplus\n{}", selection, read_to_string(format!("{pathforgit}/{selection}")).unwrap()).as_bytes()).unwrap();
-  } else {
-    let input = Question::select("theme")
+  let mut _append = true;
+  if Path::new(".gitignore").is_file() {
+    let input = Question::select("gitignorexist")
     .message(".gitignore exists, append or overwrite?")
     .choices(vec!["append", "overwrite", "cancel"])
     .build();
@@ -86,31 +83,34 @@ fn main() {
       println!("Canceled");
       return;
     }
-    let _append: bool = _selection == "append";
-    let mut file = OpenOptions::new()
-      .write(true)
-      .append(_append)
-      .open(".gitignore")
-      .unwrap();
+    _append = _selection == "append";
+  } else {
+    File::create(".gitignore").unwrap();
+  }
+  let mut file = OpenOptions::new()
+    .write(true)
+    .append(_append)
+    .open(".gitignore")
+    .unwrap();
 
-    if !_append {
-      file.set_len(0).unwrap();
-    }
+  if !_append {
+    file.set_len(0).unwrap();
+  }
 
-    let input: Question<'_> = Question::select("b")
-      .message("Do you want a delete comments in .gitignore? (y/n)")
-      .choices(vec!["y", "n"])
-      .build();
+  let input: Question<'_> = Question::select("b")
+    .message("Do you want to delete comments in .gitignore? (y/n)")
+    .choices(vec!["y", "n"])
+    .build();
 
-    let _selection: bool;
+  let _selection: bool;
 
-    if requestty::prompt_one(input).unwrap().try_into_list_item().unwrap().text == "y" {
-      _selection = true;
-    } else {
-      _selection = false;
-    }
+  if requestty::prompt_one(input).unwrap().try_into_list_item().unwrap().text == "y" {
+    _selection = true;
+  } else {
+    _selection = false;
+  }
 
-    let _path = read_to_string(format!("{pathforgit}/{selection}")).unwrap();
+  let _path = read_to_string(format!("{pathforgit}/{selection}")).unwrap();
     let strings = _path.trim_end_matches('\n');
     
     if let Err(e) = writeln!(file, "\n# {} by ignofierplus\n{}",
@@ -118,7 +118,6 @@ fn main() {
       comment_delete(strings.to_string(), _selection)
     ) {
       eprintln!("Couldn't write to file: {}", e);
-    }
   }
 
   println!("Your .gitignore ready!")
